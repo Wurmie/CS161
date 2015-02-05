1697c38068cb12c281ecbbebd731dced72d47adb
Wurmie <wlee030@ucr.edu>
2015-02-04 19:40:44 -0800

    Update My_ALU.vhd

--- CS161/lab2/My_ALU.vhd
+++ CS161/lab2/My_ALU.vhd
@@ -23,6 +23,7 @@ use ieee.std_logic_unsigned.all ;
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 use IEEE.NUMERIC_STD.ALL;
+use ieee.std_logic_arith.all;
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx primitives in this code.
 --library UNISIM;
@@ -59,15 +60,15 @@ begin
     begin
     case opcode is
         when "1000" => --unsigned add
-            total <= std_logic_vector(unsigned('0' & A) + unsigned('0' & B));
-            num0 <= std_logic_vector(unsigned(total) mod 10);
-            num1 <= std_logic_vector(unsigned(total)/10 mod 10);
-            num2 <= std_logic_vector(unsigned(total)/100 mod 10);
-            num3 <= std_logic_vector(unsigned(total)/1000 mod 10);
-            num4 <= std_logic_vector(unsigned(total)/10000 mod 10);
-            num5 <= std_logic_vector(unsigned(total)/100000 mod 10);
-            num6 <= std_logic_vector(unsigned(total)/1000000 mod 10);
-            num7 <= std_logic_vector(unsigned(total)/10000000 mod 10);
+            total <= conv_std_logic_vector(unsigned('0' & A) + unsigned('0' & B));
+            num0 <= conv_std_logic_vector(to_integer(unsigned(total))/1 mod 10);
+            num1 <= conv_std_logic_vector(to_integer(unsigned(total))/10 mod 10);
+            num2 <= conv_std_logic_vector(to_integer(unsigned(total))/100 mod 10);
+            num3 <= conv_std_logic_vector(to_integer(unsigned(total))/1000 mod 10);
+            num4 <= conv_std_logic_vector(to_integer(unsigned(total))/10000 mod 10);
+            num5 <= conv_std_logic_vector(to_integer(unsigned(total))/100000 mod 10);
+            num6 <= conv_std_logic_vector(to_integer(unsigned(total))/1000000 mod 10);
+            num7 <= conv_std_logic_vector(to_integer(unsigned(total))/10000000 mod 10);
             result <= std_logic_vector("0000" & num7 & num6 & num5 & num4 & num3 & num2 & num1 & num0);
             outTotal <= "000" & total;
 				overflow <= total(NUMBITS);
